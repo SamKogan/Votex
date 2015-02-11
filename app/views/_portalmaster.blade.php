78004c2ce6452890e200cd326edfe5447f3fc88e
Sam Kogan <samuelstolin@gmail.com>
2015-02-12 00:43:40 +0200

    fixed navbar

--- app/views/_portalmaster.blade.php
+++ app/views/_portalmaster.blade.php
@@ -34,7 +34,7 @@
     <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsablenav">
+            <button type="button" style="height: 35px; margin-top:5px; margin-bottom: 5px;" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsablenav">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
